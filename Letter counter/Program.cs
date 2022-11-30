07689bd052e966164761b5db7d6ae1c31195de3f
matty147 <88089429+matty147@users.noreply.github.com>
2022-11-30 09:30:17 +0100

    First commit

---
+++ Letter counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letter_counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int Howmuch = 0;
+            Console.WriteLine("Please enter the character you are looking for");
+            char Char = Console.ReadLine()[0];
+            Console.WriteLine("Please enter the text where you want to find the charecter");
+            string Inlook = Console.ReadLine();
+            for (int i = 0; i < Inlook.Length; i++)
+            {
+            if (Inlook[i] == Char)
+                {
+                    Howmuch++;
+                }
+            }
+            
+            Console.WriteLine(Howmuch);
+            Console.ReadKey();
+        }
+    }
+}
